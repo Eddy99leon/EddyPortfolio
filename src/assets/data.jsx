ba73f0c6ad14a17cfdfca884565ebbf3f9693d8b
Eddy Léon <leoneddy504@gmail.com>
2023-12-18 07:53:12 +0100

    update data projects

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -2,48 +2,60 @@ import onifra from "../assets/OnifraDes.jpg"
 import noteo from "../assets/NoteoDes.jpg"
 import madagascar from "../assets/MadagascarDes.jpg"
 import portfolio from "../assets/PortfolioDes.jpg"
+import EddyDev from "../assets/EddyDev.png"
 
 export const Projects = [
     {
         "id": 1,
+        "Pic": EddyDev,
+        "Nom": "EddyDev",
+        "Category": "fullstack",
+        "Description": "Ce projet un plateforme qui présente un aperçu de mes realisations, mettant en valeur mes réalisation à travers différents projets.",
+        "TechnoFront": "NextJs et Tailwind CSS",
+        "TechnoBack": "MongoDB et Mongoose",
+        "LiveLink": "https://eddy-dev.vercel.app/",
+        "CodeLink": "https://github.com/Eddy99leon/EddyDev"
+    },
+    {
+        "id": 2,
         "Pic": onifra,
         "Nom": "Onifra.mg",
         "Category": "fullstack",
         "Description": "Le projet en question concerne un site universitaire, où j'ai été chargé à la fois de concevoir la maquette et d'assurer l'intégration du frontend.",
-        "TechnoFront": "Tailwind et React",
+        "TechnoFront": "Tailwind et ReactJs",
         "TechnoBack": "MySQL et Laravel",
         "LiveLink": "https://onifra.mg",
         "CodeLink": ""
     },
     {
-        "id": 2,
+        "id": 3,
         "Pic": madagascar,
-        "Nom": "Madagascar.github.com",
+        "Nom": "Madagascar",
         "Category": "frontend",
         "Description": "Ce plateforme est une plateforme immersive dédiée à présenter les merveilles, les richesses naturelles et culturelles ainsi que les expériences uniques que Madagascar offre aux voyageurs.",
-        "TechnoFront": "Tailwind et React",
+        "TechnoFront": "Tailwind et ReactJs",
         "TechnoBack": "",
         "LiveLink": "https://eddy99leon.github.io/Madagascar/",
         "CodeLink": "https://github.com/Eddy99leon/Madagascar"
     },
     {
-        "id": 3,
+        "id": 4,
         "Pic": noteo,
         "Nom": "Noteo.grossiste.mg",
         "Category": "fullstack",
         "Description": "Noteo se présente comme une plateforme d'avis en ligne à Madagascar, offrant aux utilisateurs la possibilité de partager leurs expériences variées sur un large éventail de produits et services.",
-        "TechnoFront": "Tailwind et React",
+        "TechnoFront": "Tailwind et ReactJs",
         "TechnoBack": "MySQL et Node/Express",
         "LiveLink": "https://noteo.grossiste.mg/",
         "CodeLink": ""
     },
     {
-        "id": 4,
+        "id": 5,
         "Pic": portfolio,
-        "Nom": "EddyPortfolio.github.com",
+        "Nom": "EddyPortfolio",
         "Category": "frontend",
         "Description": "Ce site présente un aperçu de mes compétences et de ce que je sais faire, mettant en valeur mes réalisation à travers différents projets.",
-        "TechnoFront": "Tailwind et React",
+        "TechnoFront": "Tailwind et ReactJs",
         "TechnoBack": "",
         "LiveLink": "https://eddy99leon.github.io/EddyPortfolio/",
         "CodeLink": "https://github.com/Eddy99leon/EddyPortfolio"
